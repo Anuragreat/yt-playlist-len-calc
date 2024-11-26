81764cf28b6bfdcf6fce47c6b52a06b9e2c2dd08
Anurag <59769356+Anuragreat@users.noreply.github.com>
2024-11-26 12:31:27 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,67 +1,73 @@
-import datetime
-import json
-import os
-from urllib.parse import urlencode
+from datetime import timedelta
 
-import aiohttp
+import isodate
 
-
-# find out which time slice an time lies in, to decide which API key to use
-def find_time_slice():
-    time_slice = datetime.datetime.now().time().hour // 4
-    return time_slice
+from src.utils import parse
 
 
-async def call_youtube_api(url_type, api, **kwargs):
+class Video:
 
-    base_url = f"https://www.googleapis.com/youtube/v3/{url_type}"
-    url_params = {
-        "playlists": {
-            "part": "snippet",
-            "fields": "items/snippet/title,items/snippet/channelTitle",
-            "id": ",".join(kwargs.get("playlist_ids", [])),
-        },
-        "playlistItems": {
-            "part": "contentDetails",
-            "maxResults": "50",
-            "fields": "items/contentDetails/videoId,nextPageToken",
-            "playlistId": kwargs.get("playlistId"),
-            "pageToken": kwargs.get("pageToken", ""),  # Optional Page Token
-        },
-        "videos": {
-            "part": "contentDetails,statistics,snippet",
-            "id": ",".join(kwargs.get("video_ids", [])),  # List of video IDs
-            "fields": "items/contentDetails/duration,items/statistics/likeCount,"
-            "items/statistics/viewCount,items/statistics/commentCount,"
-            "items/snippet/title,items/snippet/channelTitle,items/snippet/publishedAt",
-        },
-    }
+    def __init__(self, video_id, video_data, custom_speed=None):
+        self.video_id = video_id
 
-    params = url_params.get(url_type, {})
-    if not params:
-        raise ValueError(f"Invalid URL type: {url_type}")
+        if video_id:
+            self.update_video_details(video_data)
+        else:
+            self.video_id = video_data["video_id"]
+            self.title = video_data["title"]
+            self.channel = video_data["channel"]
+            self.published = video_data["published"]
+            self.duration = isodate.parse_duration(video_data["duration"])
+            self.views = video_data["views"]
+            self.likes = video_data["likes"]
+            self.comments = video_data["comments"]
+            self.considered = video_data["considered"]
 
-    params["key"] = api
-    url = f"{base_url}?{urlencode(params, safe=',')}"
+        self.custom_speed = custom_speed
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            return json.loads(await response.text())
+    def update_video_details(self, video_data: dict):
+        self.title = video_data["snippet"]["title"]
+        self.channel = video_data["snippet"]["channelTitle"]
+        self.published = video_data["snippet"]["publishedAt"]
+        self.duration = isodate.parse_duration(
+            video_data["contentDetails"].get("duration", "PT0S")
+        )
+        self.views = video_data["statistics"].get("viewCount", 0)
+        self.likes = video_data["statistics"].get("likeCount", 0)
+        self.comments = video_data["statistics"].get("commentCount", 0)
+        self.considered = True if self.duration.total_seconds() > 0 else False
 
+    def to_dict(self) -> dict:
+        return {
+            "video_id": self.video_id,
+            "title": self.title,
+            "channel": self.channel,
+            "published": self.published,
+            "duration": isodate.duration_isoformat(
+                self.duration
+            ),  # Convert timedelta to ISO 8601 duration
+            "views": self.views,
+            "likes": self.likes,
+            "comments": self.comments,
+            "considered": self.considered,
+        }
 
-def parse(a):
-    ts, td = a.seconds, a.days
-    th, tr = divmod(ts, 3600)
-    tm, ts = divmod(tr, 60)
-    ds = ""
-    if td:
-        ds += " {} day{},".format(td, "s" if td != 1 else "")
-    if th:
-        ds += " {} hour{},".format(th, "s" if th != 1 else "")
-    if tm:
-        ds += " {} minute{},".format(tm, "s" if tm != 1 else "")
-    if ts:
-        ds += " {} second{}".format(ts, "s" if ts != 1 else "")
-    if ds == "":
-        ds = "0 seconds"
-    return ds.strip().strip(",")
+    def __repr__(self):
+        return f"Video(video_id={self.video_id}, title={self.title}, duration={self.duration})"
+
+    def get_output_string(self):
+        output_string = [
+            "Video : " + self.title,
+            "ID : " + self.video_id,
+            "Duration : " + parse(self.duration),
+            "At 1.25x : " + parse(self.duration / 1.25),
+            "At 1.50x : " + parse(self.duration / 1.5),
+            "At 1.75x : " + parse(self.duration / 1.75),
+            "At 2.00x : " + parse(self.duration / 2),
+        ]
+
+        if self.custom_speed:
+            output_string.append(
+                f"At {self.custom_speed:.2f}x : {parse(self.duration / self.custom_speed)}"
+            )
+        return output_string
